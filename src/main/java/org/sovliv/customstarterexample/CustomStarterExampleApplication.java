e5088bb56660138d7213db2c14f2daa43952c116
Vladimir <sovliv@yandex.ru>
2025-08-03 17:39:17 +0300

    first commit

---
+++ src/main/java/org/sovliv/customstarterexample/CustomStarterExampleApplication.java
@@ -0,0 +1,13 @@
+package org.sovliv.customstarterexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomStarterExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomStarterExampleApplication.class, args);
+    }
+
+}
